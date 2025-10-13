b3b7c856b861dff4db69c70a4a74aa440d5fed6f
Juan David <141162578+Jcastaho@users.noreply.github.com>
2025-10-12 23:21:01 -0500

    Añadido soporte para Tipos de Cuentas con validaciones
    Se realizaron los siguientes cambios principales:
    - Deshabilitada la característica "Nullable" en el proyecto y añadidas referencias a los paquetes `Dapper` y `Microsoft.Data.SqlClient`.
    - Registrado el servicio `IRepositorioTiposCuentas` en el contenedor de dependencias.
    - Creado el controlador `TiposCuentasController` con acciones para crear tipos de cuentas.
    - Añadido el modelo `TipoCuenta` con validaciones, incluyendo una validación personalizada para que la primera letra sea mayúscula.
    - Implementado el repositorio `RepositorioTiposCuentas` para manejar operaciones con la base de datos utilizando Dapper.
    - Configurada la cadena de conexión en `appsettings.Development.json`.
    - Añadida la vista `Crear.cshtml` para la creación de tipos de cuentas con validaciones del lado del cliente.
    - Incorporado un script adicional de validación en `_ValidationScriptsPartial.cshtml`.

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,32 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+        }
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            tipoCuenta.UsuarioId = 3;
+            await repositorioTiposCuentas.Crear(tipoCuenta);
+            return View();
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,30 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta //: IValidatableObject
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe de estar entre {2} y {1}")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if(Nombre != null && Nombre.Length > 0)
+        //    {
+        //        var primeraLetra = Nombre[0].ToString();
+        //        if (primeraLetra != primeraLetra.ToUpper())
+        //        {
+        //            yield return new ValidationResult("La primera letra debe de ser mayuscula",
+        //                new[] { nameof(Nombre) });
+
+        //        }
+        //    }
+        //}
+    }
+}
