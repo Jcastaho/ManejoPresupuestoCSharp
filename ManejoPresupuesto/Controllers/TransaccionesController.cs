89f444ea2534b9c7c69ebc3f470d8a150829cb2a
Juan David <141162578+Jcastaho@users.noreply.github.com>
2025-11-03 15:34:25 -0500

    Añadir reporte semanal y mejoras en la UI
    Se ha modificado el método `Semanal` en `TransaccionesController.cs` para que sea asíncrono y acepte parámetros `mes` y `anio`. Se ha añadido el método `ObtenerPorSemana` en `RepositorioTransacciones.cs` para obtener transacciones agrupadas por semana. En `ServicioReportes.cs`, se ha implementado `ObtenerReporteSemanal` para generar reportes semanales. Se han realizado mejoras en la UI de `Index.cshtml`, incluyendo nuevos botones para crear cuentas y transacciones. Además, se ha creado la clase `ResultadoObtenerPorSemana` para manejar los resultados de las transacciones semanales.

--- ManejoPresupuesto/Controllers/TransaccionesController.cs
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -39,8 +39,12 @@ namespace ManejoPresupuesto.Controllers
             return View(modelo);
         }
 
-        public IActionResult Semanal()
+        public async Task<IActionResult> Semanal(int mes, int anio)
         {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            IEnumerable<ResultadoObtenerPorSemana> transaccionesPorSemana = await servicioReportes.ObtenerReporteSemanal(
+                usuarioId, mes, anio, ViewBag);
+
             return View();
         }
         public IActionResult Mensual()
